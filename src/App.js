fd470b062ab34fa0d36808be188dcf9605161f60
HashimAziz <hashimkayani97@gmail.com>
2021-06-08 08:47:39 +0500

    working on submenu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import Submenu from "./Submenu";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Submenu />
     </>
   );
 }
